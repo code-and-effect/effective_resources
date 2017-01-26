c324e52066a70275fddeb12707652b9e46dfd43e
Matt Riemer <matthew@agilestyle.com>
2017-01-26 12:37:33 -0700

    check for association reflection method

--- app/models/effective/resources/associations.rb
+++ app/models/effective/resources/associations.rb
@@ -3,13 +3,13 @@ module Effective
     module Associations
 
       def belong_tos
-        return [] unless klass
+        return [] unless klass.respond_to?(:reflect_on_all_associations)
 
         @belong_tos ||= klass.reflect_on_all_associations(:belongs_to)
       end
 
       def nested_resources
-        return {} unless klass
+        return {} unless klass.respond_to?(:reflect_on_all_autosave_associations)
 
         @nested ||= klass.reflect_on_all_autosave_associations.inject({}) do |hash, reference|
           hash[reference] = Effective::Resource.new(reference); hash
