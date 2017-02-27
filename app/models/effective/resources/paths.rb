23ee24bdb8d9183d06fc42d547a76fac80cbf48d
Matt Riemer <matthew@agilestyle.com>
2017-02-27 16:53:01 -0700

    Add ability to check routes on route helpers

--- app/models/effective/resources/paths.rb
+++ app/models/effective/resources/paths.rb
@@ -3,24 +3,41 @@ module Effective
     module Paths
 
       # Controller REST helper paths
-      def index_path
-        [namespace, plural_name, 'path'].compact * '_'
+      def index_path(check: false)
+        path = [namespace, plural_name, 'path'].compact * '_'
+        path if (!check || path_exists?(path))
       end
 
-      def new_path
-        ['new', namespace, name, 'path'].compact * '_'
+      def new_path(check: false)
+        path = ['new', namespace, name, 'path'].compact * '_'
+        path if (!check || path_exists?(path))
       end
 
-      def show_path
-        [namespace, name, 'path'].compact * '_'
+      def show_path(check: false)
+        path = [namespace, name, 'path'].compact * '_'
+        path if (!check || path_exists?(path, 1))
       end
 
-      def edit_path
-        ['edit', namespace, name, 'path'].compact * '_'
+      def destroy_path(check: false)
+        path = [namespace, name, 'path'].compact * '_'
+        path if (!check || path_exists?(path, 1, :delete))
       end
 
-      def action_path(action)
-        [action, namespace, name, 'path'].compact * '_'
+      def edit_path(check: false)
+        path = ['edit', namespace, name, 'path'].compact * '_'
+        path if (!check || path_exists?(path, 1))
+      end
+
+      def action_path(action, check: false)
+        path = [action, namespace, name, 'path'].compact * '_'
+        path if (!check || path_exists?(path, 1))
+      end
+
+      def path_exists?(path, param = nil, verb = :get)
+        routes = Rails.application.routes
+
+        return false unless routes.url_helpers.respond_to?(path)
+        (routes.recognize_path(routes.url_helpers.send(path, param), method: verb).present? rescue false)
       end
 
       # _helper methods also put in the (@thing)
