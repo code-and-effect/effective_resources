227efcb83808a112f58fce65f0bfe575ba540371
Dana Janssen <dana@agilestyle.com>
2017-09-21 13:10:34 -0600

    Change %p.text-right to %div.form-actions

--- app/helpers/effective_resources_helper.rb
+++ app/helpers/effective_resources_helper.rb
@@ -1,7 +1,7 @@
 module EffectiveResourcesHelper
 
-  def simple_form_submit(form, options = {class: 'text-right'}, &block)
-    content_tag(:p, class: options[:class]) do
+  def simple_form_submit(form, options = {class: 'form-actions'}, &block)
+    content_tag(:div, class: options[:class]) do
       [
         form.button(:submit, 'Save', data: { disable_with: 'Saving...' }),
         form.button(:submit, 'Save and Continue', data: { disable_with: 'Saving...' }),
@@ -11,8 +11,8 @@ module EffectiveResourcesHelper
     end
   end
 
-  def simple_form_save(form, options = {class: 'text-right'}, &block)
-    content_tag(:p, class: options[:class]) do
+  def simple_form_save(form, options = {class: 'form-actions'}, &block)
+    content_tag(:div, class: options[:class]) do
       form.button(:submit, 'Save', data: { disable_with: 'Saving...' }) + (capture(&:block) if block_given?)
     end
   end
