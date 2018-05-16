ff8625f72f85ac46c18db1a941beaff005bd93c4
Matt Riemer <matthew@agilestyle.com>
2018-05-16 14:16:43 -0600

    acts_as_tokened global_id

--- app/models/concerns/acts_as_tokened.rb
+++ app/models/concerns/acts_as_tokened.rb
@@ -30,6 +30,7 @@ module ActsAsTokened
     def find(*args)
       return super unless args.length == 1
       return super if block_given?
+      return find_by_id(args.first) if @_effective_reloading
 
       find_by_token(args.first) || raise(::ActiveRecord::RecordNotFound.new("Couldn't find #{name} with 'token'=#{args.first}"))
     end
@@ -38,6 +39,17 @@ module ActsAsTokened
   # Instance Methods
   def to_param
     token
+  end
+
+  def to_global_id(**params)
+    GlobalID.new(URI::GID.build(app: Rails.application.config.global_id.app, model_name: model_name, model_id: to_param, params: params))
+  end
+
+  def reload(options = nil)
+    self.class.instance_variable_set(:@_effective_reloading, true)
+    retval = super
+    self.class.instance_variable_set(:@_effective_reloading, nil)
+    retval
   end
 
 end
