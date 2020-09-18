94c18fe3712a261708020e36df4c482eaab7499d
Matt Riemer <matthew@codeandeffect.com>
2020-09-18 15:04:35 -0600

    reduce allocations

--- app/helpers/effective_resources_private_helper.rb
+++ app/helpers/effective_resources_private_helper.rb
@@ -2,21 +2,27 @@
 
 module EffectiveResourcesPrivateHelper
   REPLACE_PAGE_ACTIONS = {'update' => :edit, 'create' => :new}
+  BLACKLIST = [:default, :only, :except, :if, :unless, :redirect, :success, :danger, :klass]
+
+  DATA_CONFIRM = 'data-confirm'
 
   def permitted_resource_actions(resource, actions)
     page_action = REPLACE_PAGE_ACTIONS[params[:action]] || params[:action].try(:to_sym) || :save
     executor = Effective::ResourceExec.new(self, resource)
 
-    actions.select do |commit, args|
+    actions.each_with_object({}) do |(commit, args), h|
       action = (args[:action] == :save ? (resource.new_record? ? :create : :update) : args[:action])
 
-      (args.key?(:only) ? args[:only].include?(page_action) : true) &&
-      (args.key?(:except) ? !args[:except].include?(page_action) : true) &&
-      (args.key?(:if) ? executor.instance_exec(&args[:if]) : true) &&
-      (args.key?(:unless) ? !executor.instance_exec(&args[:unless]) : true) &&
-      EffectiveResources.authorized?(controller, action, resource)
-    end.inject({}) do |h, (commit, args)|
+      permitted = (args.key?(:only) ? args[:only].include?(page_action) : true) &&
+        (args.key?(:except) ? !args[:except].include?(page_action) : true) &&
+        (args.key?(:if) ? executor.instance_exec(&args[:if]) : true) &&
+        (args.key?(:unless) ? !executor.instance_exec(&args[:unless]) : true) &&
+        EffectiveResources.authorized?(controller, action, resource)
+
+      next unless permitted
+
       opts = args.except(:default, :only, :except, :if, :unless, :redirect, :success, :danger, :klass)
+      resource_to_s = resource.to_s.presence || resource.class.name.underscore
 
       # Transform data: { ... } hash into 'data-' keys
       if opts.key?(:data)
@@ -32,34 +38,34 @@ module EffectiveResourcesPrivateHelper
       end
 
       # Replace resource name in any token strings
-      if opts.key?('data-confirm')
-        opts['data-confirm'] = opts['data-confirm'].gsub('@resource', (resource.to_s.presence || resource.class.name.gsub('::', ' ').underscore.gsub('_', ' ')).to_s)
+      if opts[DATA_CONFIRM].present? && opts[DATA_CONFIRM].include?('@resource'.freeze)
+        opts[DATA_CONFIRM] = opts[DATA_CONFIRM].gsub('@resource'.freeze, resource_to_s)
       end
 
       # Assign class
       opts[:class] ||= (
-        if opts['data-method'].to_s == 'delete'
-          'btn btn-danger'
+        if opts['data-method'.freeze] == 'delete'.freeze
+          'btn btn-danger'.freeze
         elsif h.length == 0
-          'btn btn-primary'
+          'btn btn-primary'.freeze
         elsif defined?(EffectiveBootstrap)
-          'btn btn-secondary'
+          'btn btn-secondary'.freeze
         else
-          'btn btn-default'
+          'btn btn-default'.freeze
         end
       )
 
       # Assign title
       opts[:title] ||= case opts[:action]
         when :save then commit
-        when :edit then "Edit #{resource}"
-        when :show then "#{resource}"
-        when :destroy then "Delete #{resource}"
+        when :edit then "Edit #{resource_to_s}"
+        when :show then "#{resource_to_s}"
+        when :destroy then "Delete #{resource_to_s}"
         when :index then "All #{resource.class.name.gsub('::', ' ').underscore.gsub('_', ' ').titleize.pluralize}"
-        else "#{opts[:action].to_s.titleize} #{resource}"
+        else "#{opts[:action].to_s.titleize} #{resource_to_s}"
       end
 
-      h[commit] = opts; h
+      h[commit] = opts
     end
   end
 
