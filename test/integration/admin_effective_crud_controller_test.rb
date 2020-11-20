6d74f022e21f03003073e69f120a1e0ddbacd53a
Matt Riemer <matthew@codeandeffect.com>
2020-11-20 14:59:30 -0700

    effective crud tests

--- test/integration/admin_effective_crud_controller_test.rb
+++ test/integration/admin_effective_crud_controller_test.rb
@@ -19,79 +19,123 @@ class AdminEffectiveCrudControllerTest < ActionDispatch::IntegrationTest
     assert_equal 'Admin::EffectiveThangsDatatable', @controller.view_context.assigns['datatable'].class.name
   end
 
-  # test 'new' do
-  #   get new_admin_thang_url
-  #   assert_response :success
-  #   assert_match "<form", @response.body
+  test 'new' do
+    get new_admin_thang_url
+    assert_response :success
+    assert_match "<form", @response.body
 
-  #   assert @controller.resource.kind_of?(Effective::Thang)
-  #   assert @controller.view_context.assigns['thang'].kind_of?(Effective::Thang)
-  # end
+    assert @controller.resource.kind_of?(Effective::Thang)
+    assert @controller.view_context.assigns['thang'].kind_of?(Effective::Thang)
+  end
 
-  # test 'create valid' do
-  #   post admin_thangs_url, params: { effective_thang: { title: 'Title', body: 'Body'} }
-  #   assert_redirected_to admin_thang_path(Effective::Thang.last)
-  #   assert_equal 'Successfully created Title', flash[:success]
+  test 'create valid' do
+    post admin_thangs_url, params: { effective_thang: { title: 'Title', body: 'Body'} }
+    assert_redirected_to admin_thang_path(Effective::Thang.last)
+    assert_equal 'Successfully created Title', flash[:success]
 
-  #   assert @controller.resource.kind_of?(Effective::Thang)
-  #   assert @controller.view_context.assigns['thang'].kind_of?(Effective::Thang)
-  #   assert @controller.view_context.assigns['thang'].persisted?
-  # end
+    assert @controller.resource.kind_of?(Effective::Thang)
+    assert @controller.view_context.assigns['thang'].kind_of?(Effective::Thang)
+    assert @controller.view_context.assigns['thang'].persisted?
+  end
 
-  # test 'create invalid' do
-  #   post admin_thangs_url, params: { effective_thang: { title: 'Title', body: nil} }
-  #   assert_response :success
-  #   assert_equal "Unable to create Title: body can't be blank", flash[:danger]
-  #   assert_match "<form", @response.body
+  test 'create invalid' do
+    post admin_thangs_url, params: { effective_thang: { title: 'Title', body: nil} }
+    assert_response :success
+    assert_equal "Unable to create Title: body can't be blank", flash[:danger]
+    assert_match "<form", @response.body
 
-  #   assert @controller.resource.kind_of?(Effective::Thang)
-  #   assert @controller.view_context.assigns['thang'].kind_of?(Effective::Thang)
-  #   assert @controller.view_context.assigns['thang'].errors.present?
-  # end
+    assert @controller.resource.kind_of?(Effective::Thang)
+    assert @controller.view_context.assigns['thang'].kind_of?(Effective::Thang)
+    assert @controller.view_context.assigns['thang'].errors.present?
+  end
 
-  # test 'edit' do
-  #   thang = Effective::Thang.create!(title: 'Title', body: 'Body')
+  test 'edit' do
+    thang = Effective::Thang.create!(title: 'Title', body: 'Body')
 
-  #   get edit_admin_thang_url(thang)
-  #   assert_response :success
-  #   assert_match "<form", @response.body
+    get edit_admin_thang_url(thang)
+    assert_response :success
+    assert_match "<form", @response.body
 
-  #   assert @controller.resource.kind_of?(Effective::Thang)
-  #   assert @controller.view_context.assigns['thang'].kind_of?(Effective::Thang)
-  #   assert @controller.view_context.assigns['thang'].persisted?
-  # end
+    assert @controller.resource.kind_of?(Effective::Thang)
+    assert @controller.view_context.assigns['thang'].kind_of?(Effective::Thang)
+    assert @controller.view_context.assigns['thang'].persisted?
+  end
 
-  # test 'update valid' do
-  #   thang = Effective::Thang.create!(title: 'Title', body: 'Body')
+  test 'update valid' do
+    thang = Effective::Thang.create!(title: 'Title', body: 'Body')
 
-  #   patch admin_thang_url(thang), params: { effective_thang: { id: thang.id, title: 'Title2', body: 'Body2'} }
-  #   assert_equal 'Successfully updated Title2', flash[:success]
-  #   assert_redirected_to edit_admin_thang_path(thang)
+    patch admin_thang_url(thang), params: { effective_thang: { id: thang.id, title: 'Title2', body: 'Body2'} }
+    assert_equal 'Successfully updated Title2', flash[:success]
+    assert_redirected_to edit_admin_thang_path(thang)
 
-  #   assert @controller.resource.kind_of?(Effective::Thang)
-  #   assert @controller.view_context.assigns['thang'].kind_of?(Effective::Thang)
-  #   assert @controller.view_context.assigns['thang'].persisted?
-  # end
+    assert @controller.resource.kind_of?(Effective::Thang)
+    assert @controller.view_context.assigns['thang'].kind_of?(Effective::Thang)
+    assert @controller.view_context.assigns['thang'].persisted?
+  end
 
-  # test 'update invalid' do
-  #   thang = Effective::Thang.create!(title: 'Title', body: 'Body')
+  test 'update invalid' do
+    thang = Effective::Thang.create!(title: 'Title', body: 'Body')
 
-  #   patch admin_thang_url(thang), params: { effective_thang: { id: thang.id, title: 'Title2', body: nil} }
-  #   assert_response :success
-  #   assert_equal "Unable to update Title2: body can't be blank", flash[:danger]
-  #   assert_match "<form", @response.body
+    patch admin_thang_url(thang), params: { effective_thang: { id: thang.id, title: 'Title2', body: nil} }
+    assert_response :success
+    assert_equal "Unable to update Title2: body can't be blank", flash[:danger]
+    assert_match "<form", @response.body
 
-  #   assert @controller.resource.kind_of?(Effective::Thang)
-  #   assert @controller.view_context.assigns['thang'].kind_of?(Effective::Thang)
-  #   assert @controller.view_context.assigns['thang'].errors.present?
-  # end
+    assert @controller.resource.kind_of?(Effective::Thang)
+    assert @controller.view_context.assigns['thang'].kind_of?(Effective::Thang)
+    assert @controller.view_context.assigns['thang'].errors.present?
+  end
 
-  # test 'destroy' do
-  #   thang = Effective::Thang.create!(title: 'Title', body: 'Body')
+  test 'destroy' do
+    thang = Effective::Thang.create!(title: 'Title', body: 'Body')
 
-  #   delete admin_thang_url(thang)
-  #   assert_equal 'Successfully deleted Title', flash[:success]
-  #   assert_redirected_to admin_thangs_path
-  # end
+    delete admin_thang_url(thang)
+    assert_equal 'Successfully deleted Title', flash[:success]
+    assert_redirected_to admin_thangs_path
+  end
+
+  test 'member action valid' do
+    thang = Effective::Thang.create!(title: 'Title', body: 'Body')
+
+    post approve_admin_thang_url(thang), params: { effective_thang: { id: thang.id, title: 'Title2', body: 'Body2'} }
+    assert_equal 'Successfully approved Title2', flash[:success]
+    assert_redirected_to edit_admin_thang_path(thang)
+
+    assert @controller.resource.kind_of?(Effective::Thang)
+    assert @controller.view_context.assigns['thang'].kind_of?(Effective::Thang)
+    assert @controller.view_context.assigns['thang'].persisted?
+  end
+
+  test 'member action invalid' do
+    thang = Effective::Thang.create!(title: 'Title', body: 'Body')
+
+    post approve_admin_thang_url(thang), params: { effective_thang: { id: thang.id, title: 'Title2', body: nil} }
+    assert_equal "Unable to approve Title2: body can't be blank", flash[:danger]
+    assert_redirected_to edit_admin_thang_path(thang)
+  end
+
+  test 'approve member action invalid from edit' do
+    thang = Effective::Thang.create!(title: 'Title', body: 'Body')
+
+    post approve_admin_thang_url(thang),
+      params: { effective_thang: { title: 'Title2', body: nil} },
+      headers: { 'Referer': edit_admin_thang_url(thang) }
+
+    assert_response :success
+    assert_equal "Unable to approve Title2: body can't be blank", flash[:danger]
+    assert_match "<form", @response.body
+
+    assert @controller.resource.kind_of?(Effective::Thang)
+    assert @controller.view_context.assigns['thang'].kind_of?(Effective::Thang)
+    assert @controller.view_context.assigns['thang'].errors.present?
+  end
+
+  test 'collection action' do
+    thang = Effective::Thang.create!(title: 'Title', body: 'Body')
+
+    get report_admin_thangs_url(thang)
+    assert_equal Effective::Thang.all.to_a, @controller.resources
+    assert_equal [thang], @controller.view_context.assigns['thangs']
+  end
 
 end
