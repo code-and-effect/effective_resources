8c70b4f5facd40aa2053c30fb716f45e8796f705
Matt Riemer <matthew@codeandeffect.com>
2022-11-29 09:32:48 -0700

    Acts as slug global id changes

--- app/models/concerns/acts_as_slugged.rb
+++ app/models/concerns/acts_as_slugged.rb
@@ -40,7 +40,13 @@ module ActsAsSlugged
       return super unless args.length == 1
       return super if block_given?
 
-      where(slug: args.first).first || raise(::ActiveRecord::RecordNotFound.new("Couldn't find #{name} with 'slug'=#{args.first}"))
+      reloading = instance_variable_get(:@_effective_reloading)
+      reloading ||= self.class.instance_variable_get(:@_effective_reloading)
+      reloading ||= klass.instance_variable_get(:@_effective_reloading) if respond_to?(:klass)
+
+      return find_by_id(args.first) if reloading
+
+      find_by_slug(args.first) || raise(::ActiveRecord::RecordNotFound.new("Couldn't find #{name} with 'slug'=#{args.first}"))
     end
 
     def find_by_slug_or_id(*args)
@@ -66,6 +72,18 @@ module ActsAsSlugged
 
   def to_param
     slug_was || slug
+  end
+
+  def to_global_id(**params)
+    params[:tenant] = Tenant.current if defined?(Tenant)
+    GlobalID.new(URI::GID.build(app: Rails.application.config.global_id.app, model_name: model_name, model_id: to_param, params: params))
+  end
+
+  def reload(options = nil)
+    self.class.instance_variable_set(:@_effective_reloading, true)
+    retval = super
+    self.class.instance_variable_set(:@_effective_reloading, nil)
+    retval
   end
 
 end
