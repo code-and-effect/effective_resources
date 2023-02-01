6c8300f251cf7ac963b5995f84ff068e9f85b14b
Matt Riemer <matthew@codeandeffect.com>
2023-02-01 10:56:19 -0700

    Add additional paths for effective_developer scaffolds

--- app/models/effective/resources/paths.rb
+++ app/models/effective/resources/paths.rb
@@ -8,18 +8,34 @@ module Effective
         Tenant.engine_path(tenant).sub("#{Rails.root}/", '') if tenant?
       end
 
+      # Model
       def model_file
         File.join(*[tenant_path, 'app/models', class_path, "#{name}.rb"].compact)
       end
 
+      # Controller
       def controller_file
         File.join(*[tenant_path, 'app/controllers', class_path, namespace, "#{plural_name}_controller.rb"].compact)
       end
 
+      def admin_effective_controller_file
+        File.join(*[tenant_path, 'app/controllers', namespace, "#{plural_name}_controller.rb"].compact)
+      end
+
+      # Datatable
       def datatable_file
         File.join(*[tenant_path, 'app/datatables', class_path, namespace, "#{plural_name}_datatable.rb"].compact)
       end
 
+      def effective_datatable_file
+        File.join(*[tenant_path, 'app/datatables', namespace, "effective_#{plural_name}_datatable.rb"].compact)
+      end
+
+      def admin_effective_datatable_file
+        File.join(*[tenant_path, 'app/datatables', namespace, "effective_#{plural_name}_datatable.rb"].compact)
+      end
+
+      # Views
       def view_file(action = :index, partial: false)
         File.join(*[tenant_path, 'app/views', class_path, namespace, plural_name, "#{'_' if partial}#{action}.html.haml"].compact)
       end
@@ -28,8 +44,8 @@ module Effective
         File.join(*[class_path, namespace, plural_name, action].compact)
       end
 
-      def flat_view_file(action = :index, partial: false)
-        File.join(*[tenant_path, 'app/views', class_path, plural_name, "#{'_' if partial}#{action}.html.haml"].compact)
+      def admin_effective_view_file(action = :index, partial: false)
+        File.join(*[tenant_path, 'app/views', namespace, plural_name, "#{'_' if partial}#{action}.html.haml"].compact)
       end
 
       def routes_file
