6ff2a9345c7bbbdb679380a436e35c1490793d41
Matt Riemer <matthew@codeandeffect.com>
2023-04-14 10:37:39 -0600

    Add user class filter_parameters for effective_table_with

--- app/models/concerns/effective_devise_user.rb
+++ app/models/concerns/effective_devise_user.rb
@@ -72,6 +72,38 @@ module EffectiveDeviseUser
       [:email, :password, :password_confirmation, :first_name, :last_name, :name, :login]
     end
 
+    def filter_parameters
+      [
+        :encrypted_password,
+        :reset_password_token,
+        :reset_password_sent_at,
+        :remember_created_at,
+        :sign_in_count,
+        :current_sign_in_at,
+        :last_sign_in_at,
+        :current_sign_in_ip,
+        :last_sign_in_ip,
+        :invitation_token,
+        :invitation_created_at,
+        :invitation_sent_at,
+        :invitation_accepted_at,
+        :invitation_limit,
+        :invited_by_type,
+        :invited_by_id,
+        :invitations_count,
+        :uid,
+        :provider,
+        :access_token,
+        :refresh_token,
+        :token_expires_at,
+        :avatar_url,
+        :roles_mask,
+        :confirmation_sent_at,
+        :confirmed_at,
+        :unconfirmed_email
+      ]
+    end
+
     def from_omniauth(auth, params)
       invitation_token = (params.presence || {})['invitation_token']
 
