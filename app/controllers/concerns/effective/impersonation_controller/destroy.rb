7d5ef5eb350430ba2b7452314d60e43f9f85c21d
Matt Riemer <matthew@codeandeffect.com>
2024-02-08 11:08:13 -0700

    Impersonation handle invalid session data better

--- app/controllers/concerns/effective/impersonation_controller/destroy.rb
+++ app/controllers/concerns/effective/impersonation_controller/destroy.rb
@@ -3,12 +3,18 @@ module Effective
     module Destroy
 
       def destroy
-        @user = current_user.class.find(session[:impersonation_user_id])
-        redirect_path = after_destroy_impersonate_path_for(@user)
-
         # Reset impersonation
         session[:impersonation_user_id] = nil
         session[:impersonation_original_path] = nil
+
+        # Edge case where the current_user is blank. Weird session data.
+        if current_user.blank?
+          redirect_to(root_path) and return
+        end
+
+        # The current_user exists
+        @user = current_user.class.find(session[:impersonation_user_id])
+        redirect_path = after_destroy_impersonate_path_for(@user)
 
         expire_data_after_sign_in!
         warden.session_serializer.store(@user, Devise::Mapping.find_scope!(@user))
