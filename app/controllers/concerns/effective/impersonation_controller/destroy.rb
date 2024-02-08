1b4195ffb576728ee46227dee79cb3a15c8f307d
Matt Riemer <matthew@codeandeffect.com>
2024-02-08 11:22:44 -0700

    Better impersonation

--- app/controllers/concerns/effective/impersonation_controller/destroy.rb
+++ app/controllers/concerns/effective/impersonation_controller/destroy.rb
@@ -3,18 +3,22 @@ module Effective
     module Destroy
 
       def destroy
-        # Reset impersonation
-        session[:impersonation_user_id] = nil
-        session[:impersonation_original_path] = nil
-
         # Edge case where the current_user is blank. Weird session data.
         if current_user.blank?
+          # Reset impersonation
+          session[:impersonation_user_id] = nil
+          session[:impersonation_original_path] = nil
+
           redirect_to(root_path) and return
         end
 
         # The current_user exists
         @user = current_user.class.find(session[:impersonation_user_id])
         redirect_path = after_destroy_impersonate_path_for(@user)
+
+        # Reset impersonation
+        session[:impersonation_user_id] = nil
+        session[:impersonation_original_path] = nil
 
         expire_data_after_sign_in!
         warden.session_serializer.store(@user, Devise::Mapping.find_scope!(@user))
