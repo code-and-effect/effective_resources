2bf0baaf50b2f3301d514098558b3f169ca5fd6c
Matt Riemer <matthew@codeandeffect.com>
2024-07-08 14:23:33 -0600

    Add select2 masked email helper

--- app/helpers/effective_resources_helper.rb
+++ app/helpers/effective_resources_helper.rb
@@ -323,6 +323,18 @@ module EffectiveResourcesHelper
     EffectiveResources.etsd(resource, attribute)
   end
 
+  def masked_email(resource)
+    public_email = resource.try(:public_email)
+    return public_email if public_email.present?
 
+    email = resource.email
+    return email if email.blank?
+
+    local, domain = email.split('@')
+    masked_local = local[0..1] + '*' * (local.length - 2)
+    masked_domain = domain.split('.').first[0..1] + '*' * (domain.split('.').first.length - 2) + '.' + domain.split('.').last
+
+    "#{masked_local}@#{masked_domain}"
+  end
 
 end
