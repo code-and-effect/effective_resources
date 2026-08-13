ff379b6ea6aba67dc80261153fb0411d6b078495
Matt Riemer <matthew@codeandeffect.com>
2026-08-13 14:05:49 -0600

    Use ActiveSupport::InheritableOptions for configs

--- lib/effective_resources/effective_gem.rb
+++ lib/effective_resources/effective_gem.rb
@@ -5,18 +5,6 @@
 module EffectiveGem
   extend ActiveSupport::Concern
 
-  class Config < ActiveSupport::OrderedOptions
-    def initialize(defaults = nil)
-      super()
-      @defaults = defaults
-    end
-
-    def [](key)
-      key = key.to_sym
-      key?(key) ? super(key) : @defaults&.[](key)
-    end
-  end
-
   EXCLUDED_GETTERS = [
     :config, :setup, :send_email, :parent_mailer_class,
     :deliver_method, :mailer_layout, :mailer_sender, :mailer_froms, :mailer_admin, :mailer_subject
@@ -49,8 +37,7 @@ module EffectiveGem
       namespace ||= Tenant.current if defined?(Tenant)
       namespace ||= :effective
 
-      defaults = @config[:effective] unless namespace == :effective
-      @config[namespace] ||= Config.new(defaults)
+      @config[namespace] ||= ActiveSupport::InheritableOptions.new(@config[:effective])
 
       yield(config(namespace))
 
